e4677c451b903ae0dd18b7a0db487903131c2b46
EluciOwl <Celvius@protonmail.com>
2026-08-16 18:21:17 +0200

    feat: add home screen UI
    - add home background and wordmark
    - port home button and menu layout
    - port styling with small optimizations

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,26 @@
 import "./HomeScreen.css";
+import homeImg from "../assets/images/home-wordmark.png";
 
 function HomeScreen() {
   return (
     <div className="home-screen">
-      <h1>Psyche</h1>
+      <button id="home-button">
+        <img src={homeImg} alt="Psyche" />
+      </button>
+
+      <div className="selection-menu">
+        <button id="thoughts-button" className="menu-button">
+          Thoughts
+        </button>
+
+        <button id="emotions-button" className="menu-button">
+          Emotions
+        </button>
+
+        <button id="analyze-button" className="menu-button">
+          Analyze
+        </button>
+      </div>
     </div>
   );
 }
